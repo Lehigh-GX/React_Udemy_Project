04e7bc1f8eef502d47164853c4508cad4fe61b07
Xiang Gao <gx7181@gmail.com>
2019-09-25 16:48:36 -0400

    Updates

---
+++ precache-manifest.2a8cfb5ca81b14faa6ca5e28051d81e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5bf592e1d1f5bea32de1200c8a4967",
+    "url": "/React_Udemy_Project/index.html"
+  },
+  {
+    "revision": "f8e60586f60568114c71",
+    "url": "/React_Udemy_Project/static/css/main.32b5fe4b.chunk.css"
+  },
+  {
+    "revision": "d13a353a0fe4879286ec",
+    "url": "/React_Udemy_Project/static/js/2.d395d0d5.chunk.js"
+  },
+  {
+    "revision": "f8e60586f60568114c71",
+    "url": "/React_Udemy_Project/static/js/main.e6171e99.chunk.js"
+  },
+  {
+    "revision": "a4e6296428288611bb53",
+    "url": "/React_Udemy_Project/static/js/runtime-main.677fb9d9.js"
+  }
+]);
